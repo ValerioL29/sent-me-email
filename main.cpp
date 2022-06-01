428335c8e1acbe210b38c13f7634844c80ce42c5
ValerioL29 <cheng2029@foxmail.com>
2022-06-01 19:19:47 +0800

    Update base64 codec - June 1st

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include "Config.h"
 #include "MathFunctions.h"
+#include "Base64Codec.h"
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {        // report version
@@ -20,6 +21,31 @@ int main(int argc, char *argv[]) {
     const double outputValue = mysqrt(inputValue);
     std::cout << "The square root of " << inputValue << " is " << outputValue
               << std::endl;
+
+    // Codec testings
+
+    bool all_tests_passed = true;
+    
+    std::string rest0_original = "abc";
+    std::string rest0_reference = "YWJj";
+
+    std::string rest0_encoded = base64_encode(reinterpret_cast<const unsigned char*>(rest0_original.c_str()),
+      rest0_original.length());
+    std::string rest0_decoded = base64_decode(rest0_encoded);
+
+    if (rest0_decoded != rest0_original) {
+      std::cout << "rest0_decoded != rest0_original" << std::endl;
+      all_tests_passed = false;
+    }
+    if (rest0_reference != rest0_encoded) {
+      std::cout << "rest0_reference != rest0_encoded" << std::endl;
+      all_tests_passed = false;
+    }
+
+    std::cout << "encoded:   " << rest0_encoded << std::endl;
+    std::cout << "reference: " << rest0_reference << std::endl;
+    std::cout << "decoded:   " << rest0_decoded << std::endl << std::endl;
+
     return 0;
 }
 
